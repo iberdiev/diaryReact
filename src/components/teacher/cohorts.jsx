fa74c9066484ce77a3be8c19dc98a4caecd3e374
diaryForSchools <iskender.berdiev@gmail.com>
2020-01-11 09:48:58 +0600

    Teachers and Parents with data

--- src/components/teacher/cohorts.jsx
+++ src/components/teacher/cohorts.jsx
@@ -3,28 +3,30 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 export default class Teacher_Cohorts extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : [{class_name: "10-А", pk: 4},{class_name: "11-Б", pk: 4}],
+        };
+    }
     render() {
         return (
             <div className="container d-flex justify-content-center mt-5">
                 
                 <div className="login-form col-lg-6 col-10  p-1 text-center">
                 <h6 >
-                            Классы Школы № 34
+                            Мои классы
                 </h6>
-                    <Link to="/teacher/one_cohort">
+
+                {this.state.data.map(cohort => (
+                    <Link to={{pathname: '/teacher/one_cohort', state: { cohortID: cohort.pk, className: cohort.class_name}}}>
                         <div className="card m-2 p-3 ">
                             <h5 className="">
-                                10-А
+                               {cohort.class_name}
                             </h5>
                         </div>
                     </Link>
-                    <Link to="/teacher/one_cohort">
-                        <div className="card m-2 p-3 ">
-                            <h5 className="">
-                                10-А
-                            </h5>
-                        </div>
-                    </Link>
+                ))}
                 </div>
             </div>
         )
--- src/components/teacher/one_cohort.jsx
+++ src/components/teacher/one_cohort.jsx
@@ -5,24 +5,29 @@ import { Link } from 'react-router-dom';
 export default class Teacher_One_Cohort extends Component {
     constructor(props){
         super(props);
-        this.token = localStorage.getItem('token');
-    }
+        this.state = {
+            cohort_ID: props.location.state.cohortID,
+            className: props.location.state.className,
+            data : [{class_name: "10-А", pk: 4},{class_name: "11-Б", pk: 4}],
+        };
+        
+    };
+    
     render() {
+        const hello = "hello";
+
         return(
             <div className="container d-flex justify-content-center mt-2">
                 <div className="login-form col-lg-6 col-10  p-1">
-                    
                     <h6 className="m-2 text-center">
-                        
                     </h6>
                     <div className="alert alert-primary m-1 mb-3" role="alert">
                         <h6 className="m-2 text-center">
-                            10-А класс
+                            {this.props.location.state.className} класс
                         </h6>
                         Классный руководитель: <br/>
                         <strong>Анара Женишбекова</strong>
                     </div>
-                    
                     <Link to="/teacher/teacher_student_list" >
                         <div className="card m-1">
                             <div className="p-3 text-center">
@@ -30,7 +35,6 @@ export default class Teacher_One_Cohort extends Component {
                             </div>
                         </div>
                     </Link>
-
                     <Link to="/teacher/cohort_time_table">
                         <div className="card m-1">
                             <div className="p-3 text-center">
